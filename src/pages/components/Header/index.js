aeeb9de6fb5ee85abd0f0dbe832d3acc8d7ae727
sjransom <sjransom@gmail.com>
2020-11-15 17:24:40 +0000

    inital setup, adding sass

---
+++ src/pages/components/Header/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Header() {
+  return (
+    <header>
+      <div>
+        <h1>Walks for the Weekend</h1>
+      </div>
+    </header>
+  )
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,7 @@
-import React from "react"
+import React from 'react'
+import './styles/styles.scss'
+import Header from './components/Header'
 
 export default function Home() {
-  return <div>Hello world!</div>
+  return <Header />
 }
