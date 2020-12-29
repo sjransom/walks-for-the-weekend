9a39e3be5292dc04baf9d9167e1ba6f4fe3f135a
sjransom <sjransom@gmail.com>
2020-12-29 12:03:57 +0000

    add react-helment to gatsby config

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,21 @@
 import React from 'react'
-import './styles/styles.scss'
+import { Helmet } from 'react-helmet'
+
 import Header from './components/Header'
+import NavBar from './components/NavBar'
+
+import './styles/styles.scss'
 
 export default function Home() {
-  return <Header />
+  return (
+    <>
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title>Walks for the Weekend</title>
+        <link rel="canonical" href="https://walksfortheweekend.co.uk/" />
+      </Helmet>
+      <Header />
+      <NavBar />
+    </>
+  )
 }
