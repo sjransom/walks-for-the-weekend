b2c1ef3c35713867d5e65f295fc9523b92bd32aa
sjransom <sjransom@gmail.com>
2020-12-29 16:48:48 +0000

    add NavBar component

--- src/pages/components/NavBar/index.js
+++ src/pages/components/NavBar/index.js
@@ -1,9 +1,30 @@
 import React from 'react'
+import { Link } from 'gatsby'
+
+import Menu from '../../assets/menu.svg'
 
 export default function NavBar() {
   return (
-    <div>
-      <p>test</p>
-    </div>
+    <nav>
+      <ul className="menu">
+        <li className="menu__item">
+          <Link to="/">Home</Link>
+        </li>
+        <li className="menu__item">
+          <Link to="/">About</Link>
+        </li>
+        <li className="menu__item">
+          <Link to="/">Walks</Link>
+        </li>
+        <li className="menu__item">
+          <Link to="/">Contact</Link>
+        </li>
+        <li className="menu__toggle">
+          <Link to="/">
+            <Menu />
+          </Link>
+        </li>
+      </ul>
+    </nav>
   )
 }
